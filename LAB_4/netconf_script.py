77bf2c3148425925dfab3f4035458e2ae9bded5f
AlexanderVanHovePXL <12300846@student.pxl.be>
2026-01-04 17:24:05 +0100

    github the one truth gemaakt en dat het foutmeldingen geeft

--- LAB_4/netconf_script.py
+++ LAB_4/netconf_script.py
@@ -10,19 +10,47 @@ m = manager.connect(
            hostkey_verify=False 
 
 )
-# hostname
-url = ""
-response = requests.get(url)
-reply1 = m.edit_config(target="running", config=netconf_hostname)
-print("Hostname response:")
-print(xml.dom.minidom.parseString(reply1.xml).toprettyxml())
+try:
+    # hostname
+    url = "https://raw.githubusercontent.com/AlexanderVanHovePXL/Netwerk_exp_Take-home/refs/heads/main/LAB_4/configs/netconf_hostname.xml"
+    response = requests.get(url)
 
-# Apply Loopback1
-reply2 = m.edit_config(target="running", config=netconf_interface)
-print("Loopback1 response:")
-print(xml.dom.minidom.parseString(reply2.xml).toprettyxml())
+    if response.status_code == 200:
+        hostname_config_xml = response.text
+    else:
+        print("github is wrong!!!")
+        exit(0)
 
-# Apply OSPF
-reply3 = m.edit_config(target="running", config=netconf_ospf)
-print("OSPF response:")
-print(xml.dom.minidom.parseString(reply3.xml).toprettyxml())
+    # interface loopback
+    url = "https://raw.githubusercontent.com/AlexanderVanHovePXL/Netwerk_exp_Take-home/refs/heads/main/LAB_4/configs/netconf_Loopback.xml"
+    response = requests.get(url)
+    if response.status_code == 200:
+        loopback_config_xml = response.text
+    else:
+        print("github is wrong!!!")
+        exit(0)
+
+    # ospf
+    url = "https://raw.githubusercontent.com/AlexanderVanHovePXL/Netwerk_exp_Take-home/refs/heads/main/LAB_4/configs/netconf_ospf.xml"
+    response = requests.get(url)
+    if response.status_code == 200:
+        ospf_config_xml = response.text
+    else:
+        print("github is wrong!!!")
+        exit(0)
+
+    reply1 = m.edit_config(target="running", config=hostname_config_xml)
+    print("Hostname response:")
+    print(xml.dom.minidom.parseString(reply1.xml).toprettyxml())
+
+    # Apply Loopback1
+    reply2 = m.edit_config(target="running", config=loopback_config_xml)
+    print("Loopback1 response:")
+    print(xml.dom.minidom.parseString(reply2.xml).toprettyxml())
+
+    # Apply OSPF
+    reply3 = m.edit_config(target="running", config=ospf_config_xml)
+    print("OSPF response:")
+    print(xml.dom.minidom.parseString(reply3.xml).toprettyxml())
+except Exception as e:
+    print("Fout tijdens configuratie: ", e)
